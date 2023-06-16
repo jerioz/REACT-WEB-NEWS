f7006ea7c44e862eb0fe6107ee54d92a86d8710f
jerioz <escobedoranero@gmail.com>
2023-06-16 20:47:04 +0200

    update ListNews

--- web-news/src/components/listNews/ListNews.jsx
+++ web-news/src/components/listNews/ListNews.jsx
@@ -1,28 +1,7 @@
-// import React, { useContext, useEffect } from "react";
-// import { GlobalContext } from "../../context/GlobalState";
-
-// const ListNews = () => {
-//     const { listNews, getNews } = useContext(GlobalContext);
-//     useEffect(() => {
-//         getNews();
-//     }, []);
-//     const news = listNews.map((news) => {
-//         return (
-//         <div key={news.ip}>
-//             <h1>{news.title}</h1>
-//             <p>{news.abstract}</p> 
-//         </div>
-//             );
-//         });
-       
-//     };
-
-//     export default ListNews;
-
 import React, { useEffect, useContext } from 'react'
-import axios from 'axios'
 import { Character } from '../character/Character'
 import { GlobalContext } from '../../context/GlobalState'
+import './ListNews.styles.scss'
 
 export const ListNews = () => {
    
@@ -34,9 +13,13 @@ export const ListNews = () => {
         
     return (
     <>
-    <h1>Most Read News</h1>
-    {characters.map((character) => (
-   <Character key={character.id} title={character.title} abstract={character.abstract}/>))}
+    <div>
+      <h1 className='news__title'>Most Read News</h1>
+      <div className='news__text'>
+      {characters.map((character) => (
+      <Character key={character.id} title={character.title} abstract={character.abstract}/>))}
+      </div>
+    </div>
     </>
   )
 }
